31ccd317586ab92dbb7cd0fed7b5ee5e98500449
Jack M <jackm550@googlemail.com>
2022-03-23 16:18:03 +0000

    changed image

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ export default function App() {
     <View style={styles.container}>
       <View style={styles.carContainer}>
         <ImageBackground
-          source={require("./assets/images/ModelX.jpeg")}
+          source={require("./assets/images/ModelS.jpeg")}
           style={styles.image}
         />
         <View style={styles.titles}>
